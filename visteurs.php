bf519585492776b28ccb1a608b743ee692a023e3
ksidibe948 <ksidibe948@gmail.com>
2020-11-01 13:27:25 +0100

    secdond commit

--- visteurs.php
+++ visteurs.php
@@ -16,14 +16,14 @@
           ?>
 <!-- la partie html -->
 <head>
-    <link rel="stylesheet" href="../Administration/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="Administration/bootstrap/bootstrap.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
     <?php  include 'pages/'.$page.'.php' ?>
-    <script src="../Administration/bootstrap/jquery-3.5.1.js"></script>
-    <script src="../Administration/bootstrap/bootstrap.min.js"></script>
+    <script src="Administration/bootstrap/jquery-3.5.1.js"></script>
+    <script src="Administration/bootstrap/bootstrap.min.js"></script>
 </body>
 </html>
